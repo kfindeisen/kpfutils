473b18b9d50ef52a4287677965fdb9837a80a7f3
Krzysztof Findeisen <kfindeisen@users.noreply.github.com>
2014-08-20 03:44:34 +0000

    Changed from Apache to BSD license to abide by Caltech rules.
    git-svn-id: svn://localhost/astroapps/common/trunk@171 756c44ca-fe3f-b449-bf70-ebd2656a6ddd

--- kpfutils.h
+++ kpfutils.h
@@ -5,6 +5,13 @@
  * @date Last modified February 28, 2014
  */
 
+/* Copyright 2014, California Institute of Technology.
+ *
+ * This file is licensed under the BSD 3-Clause License. It is subject to the 
+ * license terms in the LICENSE file found in the top-level directory of this 
+ * distribution and at http://opensource.org/licenses/BSD-3-Clause. 
+ */
+
 #ifndef KPFUTILSH
 #define KPFUTILSH
 
@@ -32,19 +39,36 @@
  * other C++ programs I have written, but are not intended for standalone use.
  *
  * @section legal License
+ * 
+ * @subsection kpfutils
  *
- * Copyright 2014 Krzysztof Findeisen
+ * Copyright 2014, California Institute of Technology
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at 
- * http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the BSD 3-Clause License (the "License"). You may obtain a copy 
+ * of the License at http://opensource.org/licenses/BSD-3-Clause. A copy is provided 
+ * for your convenience in plain text format in <a href="../../LICENSE">common/LICENSE</a>.
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * @subsection kpfutilstest Test Suite
+ *
+ * Copyright (C) 2014 California Institute of Technology
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, subject to the following 
+ * exception added under Section 7 of the License:
+ *	* Neither the name of the copyright holder nor the names of its contributors 
+ *	  may be used to endorse or promote products derived from this software 
+ *	  without specific prior written permission.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * in <a href="../../tests/LICENSE">common/tests/LICENSE</a> 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @page install Installation
  *
--- nan.cpp
+++ nan.cpp
@@ -5,19 +5,11 @@
  * @date Last modified October 30, 2013
  */
 
-/* Copyright 2014 Krzysztof Findeisen
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/* Copyright 2014, California Institute of Technology.
+ *
+ * This file is licensed under the BSD 3-Clause License. It is subject to the 
+ * license terms in the LICENSE file found in the top-level directory of this 
+ * distribution and at http://opensource.org/licenses/BSD-3-Clause. 
  */
 
 #include <limits>
